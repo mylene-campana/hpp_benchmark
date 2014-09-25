93595a5043d4b514a2d53c009df51dca30eabdd5
Florent Lamiraux <florent@laas.fr>
2014-09-25 09:06:26 +0200

    Add callgrind script and output.

---
+++ 2014-09-03/pr2-in-iai-kitchen/callgrind.py
@@ -0,0 +1,16 @@
+from hpp.corbaserver.pr2 import Robot
+robot = Robot ('pr2')
+robot.setJointBounds ("base_joint_x", [-4, -3])
+robot.setJointBounds ("base_joint_y", [-5, -3])
+
+from hpp.corbaserver import ProblemSolver
+ps = ProblemSolver (robot)
+
+q_init = [-3.2, -4, 0, 0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.2, 0.0, 0.0, 0.0, 0.0, 0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+
+q_goal = [-3.2, -4, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0, 0.0, 0.0, 0.0, 0.5, 0, -0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 0, -0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+
+ps.loadObstacleFromUrdf ("iai_maps", "kitchen_area")
+
+ps.selectPathOptimizer ("None")
+ps.directPath (q_init, q_goal)
