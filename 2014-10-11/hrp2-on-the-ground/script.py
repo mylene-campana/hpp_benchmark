7a393d41098958495a74645aa57b14ce5c4e9d3b
Florent Lamiraux <florent@laas.fr>
2014-10-11 13:34:22 +0200

    JointRotation has been split into 2 joints.

---
+++ 2014-10-11/hrp2-on-the-ground/script.py
@@ -0,0 +1,82 @@
+#/usr/bin/env python
+
+from hpp_ros import ScenePublisher, PathPlayer
+from hpp.corbaserver.hrp2 import Robot
+from hpp.corbaserver import ProblemSolver
+from hpp.corbaserver.wholebody_step.client import Client as WsClient
+
+Robot.urdfSuffix = '_capsule'
+Robot.srdfSuffix= '_capsule'
+
+robot = Robot ('hrp2_14')
+robot.setJointBounds ("base_joint_xyz", (-3, 3, -3, 3, 0, 1))
+cl = robot.client
+
+r = ScenePublisher (robot)
+q0 = robot.getInitialConfig ()
+r (q0)
+
+# Add constraints
+wcl = WsClient ()
+wcl.problem.addStaticStabilityConstraints ("balance", q0, robot.leftAnkle,
+                                           robot.rightAnkle)
+
+ps = ProblemSolver (robot)
+ps.setNumericalConstraints ("balance", ["balance/relative-com",
+                                                "balance/relative-orientation",
+                                                "balance/relative-position",
+                                                "balance/orientation-left-foot",
+                                                "balance/position-left-foot"])
+
+# lock hands in closed position
+lockedDofs = robot.leftHandClosed ()
+for name, value in lockedDofs.iteritems ():
+    ps.lockDof (name, value, 0, 0)
+
+lockedDofs = robot.rightHandClosed ()
+for name, value in lockedDofs.iteritems ():
+    ps.lockDof (name, value, 0, 0)
+
+
+q1 = [0.0, 0.0, 0.705, 1.0, 0., 0., 0.0, 0.0, 0.0, 0.0, 0.0, -0.4, 0, -1.2, -1.0, 0.0, 0.0, 0.174532, -0.174532, 0.174532, -0.174532, 0.174532, -0.174532, 0.261799, -0.17453, 0.0, -0.523599, 0.0, 0.0, 0.174532, -0.174532, 0.174532, -0.174532, 0.174532, -0.174532, 0.0, 0.0, -0.453786, 0.872665, -0.418879, 0.0, 0.0, 0.0, -0.453786, 0.872665, -0.418879, 0.0]
+
+res = ps.applyConstraints (q1)
+if res [0]:
+    q1proj = res [1]
+else:
+    raise RuntimeError ("Failed to apply constraint.")
+
+
+q2 = [0.0, 0.0, 0.705, 1, 0, 0, 0, 0.0, 0.0, 0.0, 0.0, 1.0, 0, -1.4, -1.0, 0.0, 0.0, 0.174532, -0.174532, 0.174532, -0.174532, 0.174532, -0.174532, 0.261799, -0.17453, 0.0, -0.523599, 0.0, 0.0, 0.174532, -0.174532, 0.174532, -0.174532, 0.174532, -0.174532, 0.0, 0.0, -0.453786, 0.872665, -0.418879, 0.0, 0.0, 0.0, -0.453786, 0.872665, -0.418879, 0.0]
+
+res = ps.applyConstraints (q2)
+if res [0]:
+    q2proj = res [1]
+else:
+    raise RuntimeError ("Failed to apply constraint.")
+
+ps.selectPathOptimizer ("None")
+ps.selectPathValidation ("Progressive", 0.025)
+
+import datetime as dt
+totalTime = dt.timedelta (0)
+totalNumberNodes = 0
+for i in range (20):
+    ps.client.problem.clearRoadmap ()
+    ps.resetGoalConfigs ()
+    ps.setInitialConfig (q1proj)
+    ps.addGoalConfig (q2proj)
+    t1 = dt.datetime.now ()
+    ps.solve ()
+    t2 = dt.datetime.now ()
+    totalTime += t2 - t1
+    print (t2-t1)
+    n = len (ps.client.problem.nodes ())
+    totalNumberNodes += n
+    print ("Number nodes: " + str(n))
+
+print ("Average time: " + str ((totalTime.seconds+1e-6*totalTime.microseconds)/20.))
+print ("Average number nodes: " + str (totalNumberNodes/20.))
+
+pp = PathPlayer (cl, r)
+pp (1)
---
+++ 2014-10-11/pr2-in-iai-kitchen/script.py
@@ -0,0 +1,58 @@
+from hpp.corbaserver.pr2 import Robot
+robot = Robot ('pr2')
+robot.setJointBounds ("base_joint_xy", [-4, -3, -5, -3])
+
+from hpp_ros import ScenePublisher
+r = ScenePublisher (robot)
+
+from hpp.corbaserver import ProblemSolver
+ps = ProblemSolver (robot)
+
+q_init = robot.getCurrentConfig ()
+q_goal = q_init [::]
+q_init [0:2] = [-3.2, -4]
+rank = robot.rankInConfiguration ['torso_lift_joint']
+q_init [rank] = 0.2
+r (q_init)
+
+q_goal [0:2] = [-3.2, -4]
+rank = robot.rankInConfiguration ['l_shoulder_lift_joint']
+q_goal [rank] = 0.5
+rank = robot.rankInConfiguration ['l_elbow_flex_joint']
+q_goal [rank] = -0.5
+rank = robot.rankInConfiguration ['r_shoulder_lift_joint']
+q_goal [rank] = 0.5
+rank = robot.rankInConfiguration ['r_elbow_flex_joint']
+q_goal [rank] = -0.5
+r (q_goal)
+
+ps.loadObstacleFromUrdf ("iai_maps", "kitchen_area", "")
+
+ps.selectPathOptimizer ("None")
+ps.selectPathValidation ("Dichotomy", 0)
+
+import datetime as dt
+totalTime = dt.timedelta (0)
+totalNumberNodes = 0
+for i in range (20):
+    ps.client.problem.clearRoadmap ()
+    ps.resetGoalConfigs ()
+    ps.setInitialConfig (q_init)
+    ps.addGoalConfig (q_goal)
+    t1 = dt.datetime.now ()
+    ps.solve ()
+    t2 = dt.datetime.now ()
+    totalTime += t2 - t1
+    print (t2-t1)
+    n = len (ps.client.problem.nodes ())
+    totalNumberNodes += n
+    print ("Number nodes: " + str(n))
+
+print ("Average time: " + str ((totalTime.seconds+1e-6*totalTime.microseconds)/20.))
+print ("Average number nodes: " + str (totalNumberNodes/20.))
+
+from hpp_ros import PathPlayer
+pp = PathPlayer (robot.client, r)
+
+pp (0)
+pp (1)
