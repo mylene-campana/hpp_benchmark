64636fe0582eab73838f3f7f84c101e72ed8a87f
Florent Lamiraux <florent@laas.fr>
2015-08-21 11:26:56 +0200

    Update script lydia.py

--- script/lydia.py
+++ script/lydia.py
@@ -1,13 +1,13 @@
 from hpp.corbaserver.lydia import Robot
 from hpp.corbaserver import ProblemSolver
-from hpp_ros import ScenePublisher
+from hpp.gepetto import Viewer, PathPlayer
 
 robot = Robot ('lydia')
 robot.setJointBounds('base_joint_xyz', [-0.9, 0.9, -0.9, 0.9, -1.1, 1.1])
 ps = ProblemSolver (robot)
-ps.loadObstacleFromUrdf ("hpp_benchmark", "obstacle", "")
+r = Viewer (ps)
 
-s = ScenePublisher (robot)
+r.loadObstacleModel ("hpp_benchmark", "obstacle", "obstacle")
 
 q_init = robot.getCurrentConfig ()
 q_goal = q_init [::]
@@ -15,6 +15,7 @@ q_goal = q_init [::]
 q_init [2] = -0.6
 q_goal [2] =  0.6
 
+ps.selectPathPlanner ("VisibilityPRM")
 ps.setInitialConfig (q_init)
 ps.addGoalConfig (q_goal)
 ps.solve ()
